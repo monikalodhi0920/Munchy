2b7717a5212a814181a22163f0816b7ffe372413
Monika Lodhi <monikalodhi10102003@gmail.com>
2025-03-25 09:11:35 +0530

    add menu section

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use('/auth', authRoutes);
 
-app.get('/', (req, res) => {
-    res.render("index");
-});
+// app.get('/', (req, res) => {
+//     res.render("index");
+// });
 
 app.get('/login', (req, res) => {
   res.render("login");  // "login.ejs" ko views folder se render karega
@@ -40,6 +40,38 @@ app.get('/login', (req, res) => {
 
 app.get('/signup', (req, res) => {
   res.render("signup"); // "signup.ejs" ko views folder se render karega
+});
+
+app.get('/', (req, res) => {
+  const menuItems = [
+      { name: "Burger", description: "Delicious cheesy burger", price: "5.99", image: "/assets/img/menu/menu-item-6.png" },
+      { name: "Pizza", description: "Hot & Spicy Pepperoni", price: "8.99", image: "/assets/img/menu/menu-item-5.png" },
+      { name: "Pasta", description: "Creamy white sauce pasta", price: "6.99", image: "/assets/img/menu/menu-item-4.png" },
+      { name: "Fries", description: "Crispy French Fries", price: "3.99", image: "/assets/img/menu/menu-item-3.png" },
+      { name: "Fries", description: "Crispy French Fries", price: "3.99", image: "/assets/img/menu/menu-item-2.png" },
+      { name: "Fries", description: "Crispy French Fries", price: "3.99", image: "/assets/img/menu/menu-item-1.png" }
+  ];
+  
+  // `index.ejs` me `menuItems` send kar raha hai
+  res.render('index', { menuItems });
+});
+
+app.get('/menu', (req, res) => {
+  const allDishes = [
+      { name: "Cheese Burger", image: "/assets/img/menu/menu-item-6.png", category: "burgers" },
+      { name: "Pepperoni Pizza", image: "/assets/img/menu/menu-item-6.png", category: "pizza" },
+      { name: "White Sauce Pasta", image: "/assets/img/menu/menu-item-6.png", category: "pasta" },
+      { name: "Chocolate Shake", image: "/assets/img/menu/menu-item-6.png", category: "drinks" },
+      { name: "Cheese Burger", image: "/assets/img/menu/menu-item-6.png", category: "burgers" },
+      { name: "Pepperoni Pizza", image: "/assets/img/menu/menu-item-6.png", category: "pizza" },
+      { name: "Cheese Burger", image: "/assets/img/menu/menu-item-6.png", category: "burgers" },
+      { name: "Pepperoni Pizza", image: "/assets/img/menu/menu-item-6.png", category: "pizza" }
+  ];
+
+  const category = req.query.category || "all";
+  const dishes = category === "all" ? allDishes : allDishes.filter(dish => dish.category === category);
+
+  res.render('menu', { dishes });
 });
 
 
